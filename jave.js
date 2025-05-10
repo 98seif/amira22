86b137f7d8946afdb9db6e1ef7531c5096f481c1
98seif <seifkarm83@gmail.com>
2025-05-10 19:08:19 +0300

    Add files via upload

---
+++ jave.js
@@ -0,0 +1,6 @@
+window.par1.style.color = "darkred";
+window.par2.style.color = "darkred";
+window.par3.style.color = "darkred";
+window.par4.style.color = "darkred";
+window.par5.style.color = "darkred";
+window.par6.style.color = "darkred";
